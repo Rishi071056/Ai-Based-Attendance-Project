7aac80ebdafdce880c4d3ae31b41e2e02e151940
Rishi <rishishukla2903@gmail.com>
2026-06-15 03:01:41 +0530

    fix env

--- src/database/config.py
+++ src/database/config.py
@@ -2,6 +2,6 @@ import os
 from supabase import create_client,Client
 
 supabase:Client=create_client(
-    os.getenv["SUPABASE_URL"],
-    os.getenv["SUPABASE_KEY"]
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_KEY")
 )
